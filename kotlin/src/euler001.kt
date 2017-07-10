07ad7564c86c7d9fbeca735605d6dfff2e5229ba
Anonymint <mint.com@gmail.com>
2017-07-10 13:11:46 -0500

    Add euler002 both java and kotlin

--- kotlin/src/euler001.kt
+++ kotlin/src/euler001.kt
@@ -5,15 +5,17 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000000000.
  */
 
-fun solution(_input: Long): Long {
-    // input is immutable so
-    val input = _input - 1
-    val sum3 = 3 * (input / 3) * (input / 3 + 1) / 2
-    val sum5 = 5 * (input / 5) * (input / 5 + 1) / 2
-    val sum15 = 15 * (input / 15) * (input / 15 + 1) / 2
-    return sum3 + sum5 - sum15
+class Euler001 {
+    fun solution(_input: Long): Long {
+        // input is immutable so
+        val input = _input - 1
+        val sum3 = 3 * (input / 3) * (input / 3 + 1) / 2
+        val sum5 = 5 * (input / 5) * (input / 5 + 1) / 2
+        val sum15 = 15 * (input / 15) * (input / 15 + 1) / 2
+        return sum3 + sum5 - sum15
+    }
 }
 
 fun main(args: Array<String>) {
-    println(solution(1_000_000_000))
+    println(Euler001().solution(1_000_000_000));
 }
